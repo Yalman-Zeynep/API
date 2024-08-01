cfa1a61489dac1646dc9a1c7bdfd2848e76d2ba1
Yalman-Zeynep <yalmanzeynep72@gmail.com>
2024-08-01 14:22:38 +0300

    .

---
+++ HelloWebAPi/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using HelloWebAPi.Models;
+
+namespace HelloWebAPi.Controllers
+{
+    [ApiController]
+    [Route("home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ResponseModel GetMessage()
+        {
+            return new ResponseModel()
+            {
+                HttpStatus = 200,
+                Message = "Hello ASP.NET Core Web API"
+            };
+        }
+
+
+
+    }
+}
